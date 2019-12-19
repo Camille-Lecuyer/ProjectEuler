93ddd02ddeec0e72f5edb5961d3cfdfbcf599be8
Camille Lecuyer <camille.lecuyer923@gmail.com>
2019-12-19 15:45:54 +0000

    first problem

---
+++ ProjectEuler/ONE.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class ONE
+    {
+        public void first()
+        {
+            int x = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    x += i;
+                }
+            }
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+
+    }
+}
